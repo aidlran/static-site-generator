cc9ce0693406bf4f64478c31b9a4da4bdb325703
Aidan Loughran <aidlo.dev@pm.me>
2022-06-18 08:09:56 +0100

    Directory existence error checking

--- build.js
+++ build.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
 const {join} = require('path');
-const FS = require('./lib/filesystem.js');
+const FS = {
+	...require('fs/promises'),
+	...require('./lib/filesystem.js')
+};
 
 const BUILD_DIR = join(__dirname, 'dist');
 const PUBLIC_DIR = join(__dirname, 'public');
 
-FS.clearDir(BUILD_DIR)
-	.then(() => FS.linkDirStructure(PUBLIC_DIR, BUILD_DIR))
+FS.mkdir(BUILD_DIR)
+	// If exists, empty it
+	.catch(() => FS.clearDir(BUILD_DIR))
+
+	// Link contents of `public` dir if present
+	.then(() => FS.exists(PUBLIC_DIR)).then(exists => {
+		if (exists) return FS.linkDirStructure(PUBLIC_DIR, BUILD_DIR);
+	})
+
 	.then(build);
 
 function build() {
